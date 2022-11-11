17ec6176faaa0e51f1abcec6ed685e7f1b368748
imStav <saprocontact@gmail.com>
2022-11-11 18:32:59 +0100

    Working on navigation

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+const { navigation } = useContent()
+</script>
+
+<template>
+    <div class="w-screen prose dark:prose-invert bg-slate-100 dark:bg-black">
+        <div class="">
+            <nav class="flex justify-center not-prose">
+                <li 
+                    v-for="link in navigation" :key="link._path"
+                    class="list-none mx-4"
+                >
+                    <NuxtLink :to="link._path">
+                        {{ link.title }}
+                    </NuxtLink>
+                </li>
+            </nav>
+        </div>
+
+        <NuxtPage />
+    </div>
+</template>

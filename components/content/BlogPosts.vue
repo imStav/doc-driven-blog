bdeb700efa37281f7bdc3bbab89fbf79590ff670
imStav <saprocontact@gmail.com>
2022-11-14 20:14:13 +0100

    Working on BlogPosts component UI
    Implemented basic light & dark mode themes.

--- components/content/BlogPosts.vue
+++ components/content/BlogPosts.vue
@@ -6,10 +6,10 @@ const { data } = await useAsyncData(() => {
 
 <template>
     <div>
-        <div v-for="post in data" :key="post._id">
+        <div v-for="post in data" :key="post._id" class="border-l border-black dark:border-white pl-6 pb-4">
             <h2>{{ post.title }}</h2>
             <p>{{ post.description }}</p>
-            <NuxtLink :to="post._path" class="no-underline">
+            <NuxtLink :to="post._path" class="no-underline px-4 py-2 bg-black prose-invert dark:bg-white dark:prose rounded-md">
                 Read more
             </NuxtLink>
         </div>

d3414d8f5934b059cfc1560d6a7820ff3c7f3508
imStav <saprocontact@gmail.com>
2022-11-19 15:38:54 +0100

    New UI for the buttons / links that lead to posts

--- components/content/BlogPosts.vue
+++ components/content/BlogPosts.vue
@@ -9,7 +9,7 @@ const { data } = await useAsyncData(() => {
         <div v-for="post in data" :key="post._id" class="border-l border-black dark:border-white pl-6 pb-4">
             <h2>{{ post.title }}</h2>
             <p>{{ post.description }}</p>
-            <NuxtLink :to="post._path" class="no-underline px-4 py-2 bg-black prose-invert dark:bg-white dark:prose rounded-md">
+            <NuxtLink :to="post._path" class="no-underline px-4 py-2 outline outline-1 rounded-md hover:outline-2">
                 Read more
             </NuxtLink>
         </div>

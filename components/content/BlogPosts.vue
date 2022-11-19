5776f12b835907ba8d24c02889320ec02f5c43bc
imStav <saprocontact@gmail.com>
2022-11-19 15:45:59 +0100

    Changed the cards border UI
    Applied an opacity filter to them, so they are less visible, and let the cards content stand out.

--- components/content/BlogPosts.vue
+++ components/content/BlogPosts.vue
@@ -6,7 +6,7 @@ const { data } = await useAsyncData(() => {
 
 <template>
     <div>
-        <div v-for="post in data" :key="post._id" class="border-l border-black dark:border-white pl-6 pb-4">
+        <div v-for="post in data" :key="post._id" class="border-l border-black/20 dark:border-white/20 px-6 pb-4">
             <h2>{{ post.title }}</h2>
             <p>{{ post.description }}</p>
             <NuxtLink :to="post._path" class="no-underline px-4 py-2 outline outline-1 rounded-md hover:outline-2">
